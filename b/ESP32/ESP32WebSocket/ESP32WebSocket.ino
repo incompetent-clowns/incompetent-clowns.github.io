adc96da95ab953398c888fd8f2b889e9175a2f4c
Atul Singh Arora <toAtulArora@gmail.com>
2026-07-22 23:31:06 +0530

    updated at Wed Jul 22 23:31:06 IST 2026

--- b/ESP32/ESP32WebSocket/ESP32WebSocket.ino
+++ b/ESP32/ESP32WebSocket/ESP32WebSocket.ino
@@ -714,12 +714,11 @@ void webSocketEvent(WStype_t type, uint8_t *payload, size_t length)
                     // JsonDocument
                     JsonDocument response;
                     response["Type"] = "device";
-                    JsonObject device_;
+                    JsonObject device_ = response["device"].to<JsonObject>();
                     device_["publicKey"] = "Working on it";
                     device_["id"] = "ESP32-1";
                     device_["signature"] = str_challenge; //"Working on it";
                     device_["Type"] = "ESP32";
-                    response["device"] = device_;
                     String str_response;
                     serializeJson(response,str_response);
                     Serial.println("Sending:" + str_response);
