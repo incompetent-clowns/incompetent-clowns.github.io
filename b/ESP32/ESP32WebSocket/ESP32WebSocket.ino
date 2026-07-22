1dc3e85954470913d985d89d5555e6a6b019c678
Atul Singh Arora <toAtulArora@gmail.com>
2026-07-22 21:01:35 +0530

    updated at Wed Jul 22 21:01:35 IST 2026

--- b/ESP32/ESP32WebSocket/ESP32WebSocket.ino
+++ b/ESP32/ESP32WebSocket/ESP32WebSocket.ino
@@ -1,18 +1,18 @@
 #include <WiFi.h>
 #include <WiFiClientSecure.h>
-// #include <ArduinoWebsockets.h>
+#include <ArduinoWebsockets.h>
 
 // #include <esp_websocket_client.h>
 
 #include "Config.h"
 
-// using namespace websockets;
+using namespace websockets;
 
 // WebsocketsClient client;
 // WiFiClient tcp;
 WiFiClientSecure tcp;
 
-// WebsocketsClient client;
+WebsocketsClient client;
 
 
 
@@ -73,13 +73,18 @@ void connectWiFi()
     // "Sec-WebSocket-Version: 13\r\n"
     // "\r\n");
 
-    Serial.println("Testing HTTPS GET");
-    tcp.print(
-        "GET / HTTP/1.1\r\n"
-        "Host: mac.taild17908.ts.net\r\n"
-        "User-Agent: ESP32\r\n"
-        "Connection: close\r\n"
-        "\r\n");
+
+
+    // Serial.println("Testing HTTPS GET");
+    // tcp.print(
+    //     "GET / HTTP/1.1\r\n"
+    //     "Host: mac.taild17908.ts.net\r\n"
+    //     "User-Agent: ESP32\r\n"
+    //     "Connection: close\r\n"
+    //     "\r\n");
+
+
+
 
     // delay(5000);
 
@@ -97,56 +102,57 @@ void connectWiFi()
 void connectWebSocket()
 {
         
-    // client.onMessage([](WebsocketsMessage message)
-    // {
-    //     Serial.print("Received: ");
-    //     Serial.println(message.data());
-    // });
+    client.onMessage([](WebsocketsMessage message)
+    {
+        Serial.print("Received: ");
+        Serial.println(message.data());
+    });
 
-    // client.onEvent([](WebsocketsEvent event, String data)
-    // {
-    //     switch(event)
-    //     {
-    //         case WebsocketsEvent::ConnectionOpened:
-    //             Serial.println("WebSocket connected");
-    //             client.send("Hello from ESP32");
-    //             break;
+    client.onEvent([](WebsocketsEvent event, String data)
+    {
+        switch(event)
+        {
+            case WebsocketsEvent::ConnectionOpened:
+                Serial.println("WebSocket connected");
+                client.send("Hello from ESP32");
+                break;
 
-    //         case WebsocketsEvent::ConnectionClosed:
-    //             Serial.println("WebSocket disconnected");
-    //             break;
+            case WebsocketsEvent::ConnectionClosed:
+                Serial.println("WebSocket disconnected");
+                break;
 
-    //         case WebsocketsEvent::GotPing:
-    //             Serial.println("Ping");
-    //             break;
+            case WebsocketsEvent::GotPing:
+                Serial.println("Ping");
+                break;
 
-    //         case WebsocketsEvent::GotPong:
-    //             Serial.println("Pong");
-    //             break;
-    //     }
-    // });
+            case WebsocketsEvent::GotPong:
+                Serial.println("Pong");
+                break;
+        }
+    });
 
-    // String url =
-    //     "wss://" +
-    //     String(WS_HOST) +
-    //     ":443/";
-    //     //":443";
+    String url =
+        "wss://" +
+        String(WS_HOST) + //"/";
+        ":443";
+        // ":443/";
+        //":443";
         
-    //     // "ws://" +
-    //     // String(WS_PATH);
-    //     // ":" +
-    //     // String(WS_PORT) +
+        // "ws://" +
+        // String(WS_PATH);
+        // ":" +
+        // String(WS_PORT) +
 
-    // Serial.print("Connecting to ");
-    // Serial.println(url);
+    Serial.print("Connecting to ");
+    Serial.println(url);
 
-    // // client.setInsecure();
-    // bool ok = client.connect(url); //,443);
+    // client.setInsecure();
+    bool ok = client.connect(url); //,443);
 
-    // if(!ok)
-    // {
-    //     Serial.println("Connection failed.");
-    // }
+    if(!ok)
+    {
+        Serial.println("Connection failed.");
+    }
 }
 
 void setup()
@@ -196,19 +202,19 @@ void loop()
     // }
 
     if (!tcp.connected()) {
-        Serial.println("Connection closed.");
+        Serial.println("TCP Connection closed.");
         delay(9000);
         // break;
     }
 
-    // if(!client.available())
-    // {
-    //     Serial.println("Reconnecting WebSocket...");
+    if(!client.available())
+    {
+        Serial.println("Reconnecting WebSocket...");
 
-    //     delay(5000);
+        delay(5000);
 
-    //     connectWebSocket();
-    // }
+        connectWebSocket();
+    }
 
     delay(1000);
 }
